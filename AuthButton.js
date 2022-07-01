cc09c684b344ee8ead03ad0871d54dd967bc8983
swatiyadav1 <108142625+swatiyadav1@users.noreply.github.com>
2022-07-01 21:45:08 +0530

    Add files via upload

---
+++ AuthButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AuthButton = props => {
+  let { isLoggedIn } = props;
+
+  if (isLoggedIn) {
+    return <button>Logout</button>;
+  } else {
+    return <button>Login</button>;
+  }
+};
+
+export default AuthButton;
